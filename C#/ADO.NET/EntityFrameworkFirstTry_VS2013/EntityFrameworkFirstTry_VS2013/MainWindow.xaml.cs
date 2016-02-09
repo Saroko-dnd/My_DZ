e5cf43a37bc7421bb8d3ac5186c690a5e1aa0aa0
unknown <admin@A2-6.itstep.lan>
2016-02-09 21:34:19 +0300

    update

--- C#/ADO.NET/EntityFrameworkFirstTry_VS2013/EntityFrameworkFirstTry_VS2013/MainWindow.xaml.cs
+++ C#/ADO.NET/EntityFrameworkFirstTry_VS2013/EntityFrameworkFirstTry_VS2013/MainWindow.xaml.cs
@@ -29,10 +29,47 @@ namespace EntityFrameworkFirstTry_VS2013
             InitializeComponent();
 
             OurLocalDataProvider = new NorthwindEntities();
-            //
-            CustomersDataGrid.DataContext = OurLocalDataProvider.Customers.Where(res => res.Country == "Mexico").
+            //Query to model of database (example)
+            //-------------------------------------
+            CustomersDataGrid.DataContext = OurLocalDataProvider.Customers.
+                Where(res => res.Country == "Mexico").
                 ToList();
+            //-------------------------------------
 
+            //adding new row to database in table 'Customers' (example)
+            //--------------------------------------------------------
+            try
+            {
+                Customers NewCustomer = new Customers()
+                {
+                    Address = "TestAddress",
+                    City = "Test",
+                    CompanyName = "Test",
+                    ContactName = "Test",
+                    ContactTitle = "Test",
+                    Country = "Test",
+                    CustomerID = "Test",
+                    Fax = "Test",
+                    Phone = "Test",
+                    PostalCode = "Test",
+                    Region = "Test"
+                };
+                OurLocalDataProvider.Customers.Add(NewCustomer);
+                OurLocalDataProvider.SaveChanges();
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+            //--------------------------------------------------------
+            Model1Container CreatedDB = new Model1Container();
+            CreatedDB.JobsSet.Add(new Jobs() {  Job = "SuperJob", Salary = 10000 });
+            CreatedDB.JobsSet.Add(new Jobs() { Job = "MegaJob", Salary = 15000 });
+            CreatedDB.JobsSet.Add(new Jobs() { Job = "EpicJob", Salary = 20000 });
+            CreatedDB.PeoplesSet.Add(new Peoples() { JobId = 1, Email = "Email_1", Name = "Name_1", SpecialCode = 19 });
+            CreatedDB.PeoplesSet.Add(new Peoples() { JobId = 1, Email = "Email_2", Name = "Name_2", SpecialCode = 67 });
+            CreatedDB.PeoplesSet.Add(new Peoples() { JobId = 1, Email = "Email_3", Name = "Name_3", SpecialCode = 34 });
+            CreatedDB.SaveChanges();
         }
 
         public void DataWindow_Closing(object sender, CancelEventArgs e)
