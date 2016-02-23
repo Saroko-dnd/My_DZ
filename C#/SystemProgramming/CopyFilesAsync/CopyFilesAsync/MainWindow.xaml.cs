732bf52a0a34b9cfb77e0767c7836572d3764509
unknown <admin@A2-6.itstep.lan>
2016-02-23 21:28:46 +0300

    update for system programming

---
+++ C#/SystemProgramming/CopyFilesAsync/CopyFilesAsync/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+//for open dialog
+using Microsoft.Win32;
+using System.Threading;
+
+namespace CopyFilesAsync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static OpenFileDialog FileSelectionDialogFrom = new OpenFileDialog();
+        public static OpenFileDialog FileSelectionDialogWhere = new OpenFileDialog();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CopyFileAsyncFunction()
+        {
+            
+        }
+
+        private void WhereButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileSelectionDialogWhere.ShowDialog();
+        }
+
+        private void FromButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileSelectionDialogFrom.ShowDialog();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Thread FileCopyThread = new Thread(CopyFile);
+            FileCopyThread.Start();
+        }
+
+        public void CopyFile()
+        {
+            
+            File.Copy(FileSelectionDialogFrom.FileName, FileSelectionDialogWhere.FileName);
+        }
+    }
+}
---
+++ C#/SystemProgramming/ThreadsSafetyExample_1/ThreadsSafetyExample_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadsSafetyExample_1
+{
+    class Program
+    {     
+        class worker
+        {
+            public static int n = 0;
+            public static int n_2 = 0;
+            public static object SuperLock = new object();
+            public static object SuperLock_2 = new object();
+            int i = 0;
+            void func()
+            {
+                while (i != 10)
+                {
+                    //залочиваем FirstLock чтобы другие workers не могли 
+                    //делать ++n пока его делает текущий экземпляр workera
+                    lock (SuperLock)
+                    {
+                        ++n;
+                    }
+                    lock (SuperLock_2)
+                    {
+                        ++n_2;
+                    }
+                    
+                    ++i;
+                    //Console.WriteLine(n);
+                }
+            }
+            public worker()
+            {
+                Thread thread = new Thread(func);
+                thread.Start();
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            worker NewWorker_1 = new worker();
+            worker NewWorker_2 = new worker();
+            Console.ReadLine();
+            Console.WriteLine(worker.n);
+            //передаем параметр через лямбда функцию
+            /*Thread FirstThread = new Thread(() => PrintIncrement(1));
+            Thread SecondThread = new Thread(() => PrintIncrement(2));*/
+            //приложение может быть завершено даже если Background поток продолжает работать
+            /*FirstThread.IsBackground = true;
+            FirstThread.Start();
+            SecondThread.Start();*/
+            Console.ReadLine();
+        }
+
+        public static void PrintIncrement(int num)
+        {
+            int countr = 0;
+            while (countr != 10)
+            {
+                Console.WriteLine(countr.ToString() + " " + num.ToString());
+                ++countr;
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
