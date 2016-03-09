9092a8da0d24d77eeeb6e42e3f99efaed9154b32
saroko-dnd <isaroko@mail.ru>
2016-03-09 03:49:11 +0300

    MSIA test program

---
+++ C#/MSIA_LINQ_examples/WPF_examples_linq/WPF_examples_linq/DataClass.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_examples_linq
+{
+    public class DataClass
+    {
+        private string name;
+        private int year;
+        private string relatedCommand;
+        private int weight;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                year = value;
+            }
+        }
+
+        public string RelatedCommand
+        {
+            get
+            {
+                return relatedCommand;
+            }
+
+            set
+            {
+                relatedCommand = value;
+            }
+        }
+
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+
+            set
+            {
+                weight = value;
+            }
+        }
+
+        public DataClass(string NewName, int NewYear,string NewCommand,int NewWeight)
+        {
+            Name = NewName;
+            Year = NewYear;
+            RelatedCommand = NewCommand;
+            Weight = NewWeight;
+        }
+    }
+}
---
+++ C#/MSIA_LINQ_examples/WPF_examples_linq/WPF_examples_linq/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_examples_linq
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<DataClass> DataStorage = new List<DataClass>();
+            DataStorage.Add(new DataClass("Kevin",30, "Immortals", 110));
+            DataStorage.Add(new DataClass("Max", 30, "Immortals", 100));
+            DataStorage.Add(new DataClass("Kelly", 40, "Immortals", 100));
+            DataStorage.Add(new DataClass("Henry", 40, "Immortals", 110));
+            DataStorage.Add(new DataClass("Andrew", 32, "Geeks", 115));
+            DataStorage.Add(new DataClass("Saladin", 32, "Geeks", 95));
+            DataStorage.Add(new DataClass("Larry", 32, "Geeks", 95));
+            DataStorage.Add(new DataClass("Ray", 35, "Geeks", 95));
+            DataStorage.Add(new DataClass("Edward", 44, "Fools", 99));
+            DataStorage.Add(new DataClass("Justin", 50, "Fools", 101));
+            DataStorage.Add(new DataClass("John", 44, "Fools", 99));
+            DataStorage.Add(new DataClass("Ryan", 40, "Fools", 99));
+
+            QueryResultDataGrid.ItemsSource = DataStorage;
+        }
+    }
+}
