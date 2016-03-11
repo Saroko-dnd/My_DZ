3c8243f02a005a2a201de6fc3626101982719fb8
unknown <admin@A2-6.itstep.lan>
2016-03-11 21:27:36 +0300

    winapi strikes back

---
+++ C#/SystemProgramming/RegistryEditProgram/RegistryEditProgram/AutoRunProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Design;
+using System.ComponentModel;
+
+namespace RegistryEditProgram
+{
+    public class AutoRunProgram
+    {
+        //[DisplayName(MyResourses.FirstTabTexts.ProgramName)]
+        public string ProgramName;
+    }
+}
