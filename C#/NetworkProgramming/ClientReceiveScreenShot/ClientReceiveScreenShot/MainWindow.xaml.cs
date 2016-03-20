f11fbd804136aec7ff99ea818a390c69acb5d0f4
saroko-dnd <isaroko@mail.ru>
2016-03-20 04:24:44 +0300

    screenshot send receive programs

---
+++ C#/NetworkProgramming/ClientReceiveScreenShot/ClientReceiveScreenShot/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ClientReceiveScreenShot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public TcpListener ReceiverOfScreenshots = new TcpListener(6000);
+        public bool ClientShutDown = false;
+        public NetworkStream StreamForScreenshots;
+        public Bitmap BitmapForScreenshot = null;
+
+        [DllImport("gdi32")]
+        public static extern int DeleteObject(IntPtr ipObj);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closing += ShutDown;
+            ReceiverOfScreenshots.Start();
+            ThreadPool.QueueUserWorkItem(o => ReceivingScreenshotsFromServer());
+        }
+
+        public void ShutDown(Object sender, EventArgs e)
+        {
+            ClientShutDown = true;
+            ReceiverOfScreenshots.Stop();
+            StreamForScreenshots.Close();
+        }
+
+        public void ReceivingScreenshotsFromServer()
+        {
+            try
+            {
+                IPEndPoint SenderIPEndPoint = null;
+
+                StreamForScreenshots = ReceiverOfScreenshots.AcceptTcpClient().GetStream();
+                byte[] BufferForBitmap = new byte[9000000];
+                while (true)
+                {
+                    StreamForScreenshots.Read(BufferForBitmap,0, BufferForBitmap.Length);
+                    if (BitmapForScreenshot != null)
+                    {
+                        BitmapForScreenshot.Dispose();
+                    }
+                    using (MemoryStream ms = new MemoryStream(BufferForBitmap))
+                    {
+                        Bitmap Screenshot = (Bitmap)Image.FromStream(ms);
+                        BitmapForScreenshot = new Bitmap(Screenshot);
+                        Screenshot.Dispose();
+                        IntPtr PointerToBitmap = BitmapForScreenshot.GetHbitmap();
+                        Action MyDelegate = new Action(() => ScreenShotImage.Source = Imaging.CreateBitmapSourceFromHBitmap(
+                            PointerToBitmap,
+                            IntPtr.Zero,
+                            Int32Rect.Empty,
+                            BitmapSizeOptions.FromEmptyOptions()));
+                        Application.Current.Dispatcher.Invoke(MyDelegate);
+                        DeleteObject(PointerToBitmap);
+                    }
+                }
+            }
+            catch (Exception CurrentException)
+            {
+                if (!ClientShutDown)
+                {
+                    MessageBox.Show(CurrentException.Message + "CLIENT", MyResourses.Texts.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+        //Метод возвращает скриншот экрана для Image control
+        private static BitmapSource CopyScreen()
+        {
+            using (var screenBmp = new Bitmap(
+                (int)SystemParameters.PrimaryScreenWidth,
+                (int)SystemParameters.PrimaryScreenHeight,
+                PixelFormat.Format32bppArgb))
+            {
+                using (var bmpGraphics = Graphics.FromImage(screenBmp))
+                {
+                    bmpGraphics.CopyFromScreen(0, 0, 0, 0, screenBmp.Size);
+                    IntPtr PointerToBitmap = screenBmp.GetHbitmap();
+                    BitmapSource Result = Imaging.CreateBitmapSourceFromHBitmap(
+                        PointerToBitmap,
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                    DeleteObject(PointerToBitmap);
+                    return Result;
+                }
+            }
+        }
+    }
+}
---
+++ C#/NetworkProgramming/ServerSendingScreenshots/ServerSendingScreenshots/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ServerSendingScreenshots
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static IPEndPoint TestClientIPEndPoint = new IPEndPoint(IPAddress.Parse("192.168.100.3"), 6000);
+        public TcpClient ScreenshotSender = new TcpClient();
+        public NetworkStream SendingStream;
+        public bool ServerShutDown = false;
+        public static IntPtr PointerToBitmap;
+
+        [DllImport("gdi32")]
+        public static extern int DeleteObject(IntPtr ipObj);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            this.Closing += ShutDown;
+            ScreenshotSender.Connect(TestClientIPEndPoint);
+            ThreadPool.SetMinThreads(2, 2);
+            ThreadPool.QueueUserWorkItem(o => ScreenshotSendingToClients());
+        }
+
+        public void ShutDown(Object sender, EventArgs e)
+        {
+            ServerShutDown = true;
+            ScreenshotSender.Close();
+            SendingStream.Close();
+        }
+
+        public void ScreenshotSendingToClients()
+        {
+            try
+            {
+                IPEndPoint ClientTestEndPoint = new IPEndPoint(IPAddress.Parse("192.168.100.3"), 6000);
+                ImageConverter converter = new ImageConverter();
+                SendingStream = ScreenshotSender.GetStream();
+                byte[] ScreenshotByteArray;
+                while (true)
+                {
+                    ScreenshotByteArray = CopyScreen();
+                    SendingStream.Write(ScreenshotByteArray, 0, ScreenshotByteArray.Length);
+                    Thread.Sleep(1000);
+                    DeleteObject(PointerToBitmap);
+                }
+            }
+            catch (Exception CurrentException)
+            {
+                if (!ServerShutDown)
+                {
+                    MessageBox.Show(CurrentException.Message + "SERVER", MyResourses.Texts.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        public static BitmapSource ConvertBitmap(Bitmap source)
+        {
+            return System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                          source.GetHbitmap(),
+                          IntPtr.Zero,
+                          Int32Rect.Empty,
+                          BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        public static Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap;
+            using (var outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
+        }
+
+        private static byte[] CopyScreen()
+        {
+            using (var screenBmp = new Bitmap(
+                (int)SystemParameters.PrimaryScreenWidth,
+                (int)SystemParameters.PrimaryScreenHeight,
+                PixelFormat.Format32bppArgb))
+            {
+                using (var bmpGraphics = Graphics.FromImage(screenBmp))
+                {
+                    bmpGraphics.CopyFromScreen(0, 0, 0, 0, screenBmp.Size);
+                    PointerToBitmap = screenBmp.GetHbitmap();
+                    BitmapSource ScreenSource = Imaging.CreateBitmapSourceFromHBitmap(
+                        PointerToBitmap,
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                    Bitmap ScreenShot = BitmapFromSource(ScreenSource);
+                    Bitmap bm3 = new Bitmap(ScreenShot);
+                    ScreenShot.Dispose();
+                    byte[] BufForScreenshot;
+                    using (MemoryStream imgStream = new MemoryStream())
+                    {
+                        bm3.Save(imgStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                        BufForScreenshot = imgStream.ToArray();
+                    }
+                    bm3.Dispose();
+                    bmpGraphics.Dispose();
+                    return BufForScreenshot;
+                }
+            }
+        }
+    }
+}
