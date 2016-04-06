f383002425bd810f9a48a4dacfab32a8e333120c
saroko-dnd <isaroko@mail.ru>
2016-04-06 17:29:51 +0300

    update for MSIA dynamic LINQ example

---
+++ C#/MSIA_LINQ_examples/DynamicLINQexample/DynamicLINQexample/HeightConverterForDataGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DynamicLINQexample
+{
+    public class HeightConverterForDataGrid : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            double BufForResult = ((double)value)/2.0;
+            return BufForResult;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
