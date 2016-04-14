493891a5fcb1903941d1be60a4aa9e484e208164
unknown <admin@A2-6.itstep.lan>
2016-04-14 21:26:06 +0300

    update for UML diagrams

---
+++ C#/tests/InheritageTest/InheritageTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface TestInterface
+    {
+        public int FirstInt;
+        public int SecondInt;
+    }
+
+    public class TestClass : TestInterface
+    {
+
+    }
+}
