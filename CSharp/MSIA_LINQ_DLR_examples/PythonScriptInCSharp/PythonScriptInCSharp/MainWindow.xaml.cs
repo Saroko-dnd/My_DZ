d39864915ad3c7f5c424ff19aef2c460e2043d96
saroko-dnd <isaroko@mail.ru>
2016-04-24 04:29:36 +0300

    update for MSIA PythonInCSharp test program

---
+++ CSharp/MSIA_LINQ_DLR_examples/PythonScriptInCSharp/PythonScriptInCSharp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IronPython.Runtime;
+using IronPython;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace PythonScriptInCSharp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Создаем Runtime для Python
+            ScriptEngine PythonEngine = Python.CreateEngine();
+            //Загружаем Python скрипт
+            dynamic PythonScript = PythonEngine.ExecuteFile("Customer.py.txt");
+            //Получаем объект класса описанного в скрипте 
+            dynamic customer = PythonScript.GetNewCustomer(99, "Fred", "111");
+            //Получение строки из скрипта
+            PythonTextBox.Text = PythonScript.PrintHelloFromPython();
+        }
+    }
+}
