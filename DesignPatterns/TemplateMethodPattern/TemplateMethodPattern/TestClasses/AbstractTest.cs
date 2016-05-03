6a15b0206e42583ceaecd6152972da84c114aecb
unknown <admin@A2-6.itstep.lan>
2016-05-03 21:22:25 +0300

    update for TemplateMethodPattern program

---
+++ DesignPatterns/TemplateMethodPattern/TemplateMethodPattern/TestClasses/AbstractTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TemplateMethodPattern.TestClasses
+{
+    public abstract class AbstractTest
+    {
+        protected int ResultValue;
+        public void PerformTest()
+        {
+            StartTest();
+            TestItself();
+            TestResults();
+        }
+
+        protected abstract void StartTest();
+        protected abstract void TestItself();
+        protected abstract void TestResults();
+
+    }
+}
---
+++ DesignPatterns/TemplateMethodPattern/TemplateMethodPattern/TestClasses/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TemplateMethodPattern.TestClasses
+{
+    class Test : AbstractTest
+    {
+        protected override void StartTest()
+        {
+            MessageBox.Show("Test started");
+        }
+
+        protected override void TestItself()
+        {
+            MessageBox.Show("Test procssing");
+            Random NewTestRandom = new Random();
+            for (int Counter = 0; Counter < 10; ++Counter)
+            {
+                ResultValue = NewTestRandom.Next(1,10);
+            }
+        }
+        protected override void TestResults()
+        {
+            if (ResultValue > 50)
+            {
+                MessageBox.Show("Success!");
+            }
+            else
+            {
+                MessageBox.Show("Failure!!");
+            }
+
+        } 
+    }
+}
