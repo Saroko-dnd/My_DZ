9a8037a2541e91d1acb442184fcc33d183d6a51e
saroko-dnd <isaroko@mail.ru>
2016-06-07 17:30:19 +0300

    added ViewModel and Model to Booking program (software testing)

---
+++ TDD/ProgramForBookingWithoutBug/ProgramForBookingWithoutBug/BookingModel.cs
@@ -0,0 +1,32 @@
+﻿using ProgramForBookingWithoutBug.DataBaseClasses;
+using ProgramForBookingWithoutBug.Resources;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramForBookingWithoutBug
+{
+    public static class BookingModel
+    {
+        /// <summary>
+        /// Возвращает список имен станций из базы данных.
+        /// </summary>
+        /// <returns></returns>
+        public static ObservableCollection<string> GetNamesOfStations()
+        {
+            ObservableCollection<string> AllStations = new ObservableCollection<string>();
+            using (ContextForBookingDataBase BookingDBcontext = new ContextForBookingDataBase(NamesOfVariables.ConnectionStringName))
+            {
+                foreach (Station CurrentStation in BookingDBcontext.ListOfStations)
+                {
+                    AllStations.Add(CurrentStation.StationName);
+                }
+            }
+
+            return AllStations;
+        }
+    }
+}
--- TDD/ProgramForBookingWithoutBug/ProgramForBookingWithoutBug/BookingViewModel.cs
+++ TDD/ProgramForBookingWithoutBug/ProgramForBookingWithoutBug/BookingViewModel.cs
@@ -1,44 +1,152 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace ProgramForBookingWithoutBug
 {
     public class BookingViewModel : INotifyPropertyChanged
     {
-        private string testString;
+        private ObservableCollection<string> allStations;
 
-        public string TestString
+        private Visibility departureStationNotFoundLabelVisibility;
+        private Visibility destinationStationNotFoundLabelVisibility;
+
+        private Visibility searchDataAnimatedEllipseVisibility;
+        private bool searchDataAnimationBegin;
+
+        private string programStateLabelContent;
+        private Brush programStateLabelForeground;
+
+        public ObservableCollection<string> AllStations
         {
             get
             {
-                return testString;
+                return allStations;
             }
 
             set
             {
-                testString = value;
+                allStations = value;
+            }
+        }
+
+        public Visibility DepartureStationNotFoundLabelVisibility
+        {
+            get
+            {
+                return departureStationNotFoundLabelVisibility;
+            }
+
+            set
+            {
+                departureStationNotFoundLabelVisibility = value;
                 NotifyPropertyChanged();
             }
         }
 
-        public BookingViewModel()
+        public Visibility DestinationStationNotFoundLabelVisibility
         {
-            TestString = "Ненавижу";
+            get
+            {
+                return destinationStationNotFoundLabelVisibility;
+            }
+
+            set
+            {
+                destinationStationNotFoundLabelVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string ProgramStateLabelContent
+        {
+            get
+            {
+                return programStateLabelContent;
+            }
+
+            set
+            {
+                programStateLabelContent = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Brush ProgramStateLabelForeground
+        {
+            get
+            {
+                return programStateLabelForeground;
+            }
+
+            set
+            {
+                programStateLabelForeground = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Visibility SearchDataAnimatedEllipseVisibility
+        {
+            get
+            {
+                return searchDataAnimatedEllipseVisibility;
+            }
+
+            set
+            {
+                searchDataAnimatedEllipseVisibility = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool SearchDataAnimationBegin
+        {
+            get
+            {
+                return searchDataAnimationBegin;
+            }
+
+            set
+            {
+                searchDataAnimationBegin = value;
+                NotifyPropertyChanged();
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-
+        /// <summary>
+        /// Вызывается когда класс BookingViewModel должен сообщить классу MainWindow об изменении значения конкретного свойства,  чтобы изменить 'привязанное' к нему значения в классе MainWindow
+        /// (В данном случае об изменении свойства вызвавшего этот метод)
+        /// </summary>
+        /// <param name="PropertyName"></param>
         public void NotifyPropertyChanged([CallerMemberName] string PropertyName = "")
         {
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
             }
+        }
+
+        public BookingViewModel()
+        {
+            AllStations = BookingModel.GetNamesOfStations();
+
+            DepartureStationNotFoundLabelVisibility = Visibility.Collapsed;
+            DestinationStationNotFoundLabelVisibility = Visibility.Collapsed;
+
+            ProgramStateLabelContent = string.Empty;
+            ProgramStateLabelForeground = Brushes.Orange;
+
+            SearchDataAnimatedEllipseVisibility = Visibility.Collapsed;
+            SearchDataAnimationBegin = false;
         }
     }
 }
