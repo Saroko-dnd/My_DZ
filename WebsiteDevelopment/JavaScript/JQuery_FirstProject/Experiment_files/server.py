a8b303320142c404fed12cdf11727430995aae8d
unknown <admin@A2-6.itstep.lan>
2016-08-18 21:25:48 +0300

    created first JQuery project

---
+++ WebsiteDevelopment/JavaScript/JQuery_FirstProject/Experiment_files/server.py
@@ -0,0 +1,1 @@
+var __server_py_script_name__ = "\u0073\u0065\u0072\u0076\u0065\u0072\u002e\u0070\u0079";
---
+++ WebsiteDevelopment/JavaScript/JQuery_FirstProject/JSfolder/MainJavaScript.js
@@ -0,0 +1,66 @@
+﻿
+$(document).ready(function (){
+    $("#HideButton_1").click(function ()
+    {
+        $('.FirstHide').hide();
+    }
+)
+}
+)
+
+
+$(document).ready(function () {
+    $("#HideButton_2").click(function () {
+        $('#UniqueP').hide();
+    }
+)
+}
+)
+
+$(document).ready(function () {
+    $("#ChangeColorButtonID").click(function () {
+        $('div p:nth-of-type(odd)').css('color','blue');
+    }
+)
+}
+)
+
+$(document).ready(function () {
+    $("#HideMsoNormalButtonID").click(function () {
+        $('.MsoNormal:nth-of-type(odd)').hide();
+    }
+)
+}
+)
+
+$(document).ready(function () {
+    $("#ShowMsoNormalButtonID").click(function () {
+        $('.MsoNormal:nth-of-type(odd)').show();
+    }
+)
+}
+)
+
+$(document).ready(function () {
+    var $images = $("table img");
+    $images.mouseenter(function () {
+        $(this).hide();
+    })
+}
+)
+
+var FadeMustBe = true;
+
+$(document).ready(function () {
+    $('#StartFadingButton').click(function()
+    {
+        while (FadeMustBe)
+        {
+            $('#FirstFadeImage').fadeIn(2000);
+            $('#SecondFadeImage').fadeOut(2000);
+            $('#FirstFadeImage').fadeOut(2000);
+            $('#SecondFadeImage').fadeIn(2000);
+        }
+    })
+}
+)
