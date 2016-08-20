942036bd7325e6991d4221ce4ef81c6d388ad2c9
saroko-dnd <isaroko@mail.ru>
2016-08-20 05:12:53 +0300

    update for JQuery test project

--- WebsiteDevelopment/JavaScript/JQuery_FirstProject/JSfolder/JSForIndividual4.js
+++ WebsiteDevelopment/JavaScript/JQuery_FirstProject/JSfolder/JSForIndividual4.js
@@ -1,1 +1,20 @@
 ﻿
+
+//Array.eq(index) - way to operate with element of array as with JQuery object
+$(document).ready(function () {
+    $("#StartMoveParagraphsButtonID").click(function () {
+        var ArrayOfParagraphs = $('p');
+        ArrayOfParagraphs.eq(2).animate({ left: '-=100px' });
+        ArrayOfParagraphs.eq(4).animate({ left: '-=100px' });
+    }
+)
+}
+)
+
+$(document).ready(function () {
+    $("img").click(function () {
+        $(this).animate({ top: '+=30px' });
+    }
+)
+}
+)
