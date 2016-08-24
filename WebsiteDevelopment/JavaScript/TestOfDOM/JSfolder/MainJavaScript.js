870efc09bad96b37a629ba4a3854bbded019dbfe
unknown <admin@A2-6.itstep.lan>
2016-08-24 21:24:33 +0300

    update for js (DOM)

---
+++ WebsiteDevelopment/JavaScript/TestOfDOM/JSfolder/MainJavaScript.js
@@ -0,0 +1,28 @@
+﻿
+window.onload = CreateListFromDivStructure;
+
+function CreateListFromDivStructure()
+{
+    var BodyOfDocument = window.document.body;
+    CurrentBodyOfDocument.appendChild(GetHtmElementAsText(BodyOfDocument));
+}
+
+function GetHtmElementAsText(CurrentBodyOfDocument)
+{
+    var CurrentList = document.createElement('UL');
+    var NodesForCurrentElement = CurrentBodyOfDocument.childNodes;
+    for (var Counter = 0; Counter < NodesForCurrentElement.length; ++Counter )
+    {
+        if (NodesForCurrentElement[Counter].nodeType == 1)
+        {
+            var NewElementOfList = document.createElement('LI');
+            NewElementOfList.appendChild(NodesForCurrentElement[Counter].tagName);
+            CurrentList.appendChild(NewElementOfList);
+            if (NodesForCurrentElement[Counter].childNodes.length > 0)
+            {
+                NewElementOfList.appendChild(GetHtmElementAsText(NodesForCurrentElement[Counter]));
+            }
+        }
+    }
+    return CurrentList;
+}
