1ee8662d704a137d90ce108e8a03c46700ae7fb2
unknown <admin@A2-6.itstep.lan>
2016-09-05 21:25:09 +0300

    update for js folder

---
+++ WebsiteDevelopment/JavaScript/JQueryLetters/JSfolder/MainJavaScript.js
@@ -0,0 +1,46 @@
+﻿ 
+
+$(document).ready(function () {
+    $('img').draggable(
+        {
+            drag: function () {
+                $(this).css("z-index","11");
+            }
+        });
+})
+
+$(document).ready(function () {
+    $('.Department, #DivWithNewLetters').droppable(
+        {
+            drop: function (event, ui) {
+                ui.draggable.css("left", '0px');
+                ui.draggable.css("top", '0px');
+                ui.draggable.css("position", 'relative');
+                $(this).css("background-color", "yellow").append(ui.draggable);
+            }
+        });
+})
+
+$(document).ready(function () {
+    $('#MainDivID').droppable(
+        {
+            drop: function (event, ui) {
+                ui.draggable.css("left", '0px');
+                ui.draggable.css("top", '0px');
+                ui.draggable.css("position", 'relative');
+                $(this).children("div.first").append(ui.draggable);
+            }
+        });
+})
+
+$(document).ready(function () {
+    $('body').droppable(
+        {
+            drop: function (event, ui) {
+                ui.draggable.css("left", '0px');
+                ui.draggable.css("top", '0px');
+                ui.draggable.css("position", 'relative');
+                $(this).children("div > div.first").append(ui.draggable);
+            }
+        });
+})
