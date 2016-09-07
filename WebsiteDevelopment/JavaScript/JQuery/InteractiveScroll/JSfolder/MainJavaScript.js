b6614eba3478efa32d9101637e11b915e51c19bb
unknown <admin@A2-6.itstep.lan>
2016-09-07 21:23:34 +0300

    update for jquery folder

---
+++ WebsiteDevelopment/JavaScript/JQuery/InteractiveScroll/JSfolder/MainJavaScript.js
@@ -0,0 +1,32 @@
+﻿
+var NumberOfAnumation = 1;
+var AllImages;
+var MainDiv;
+
+/*window.onresize = ResizeBodyHeight;
+
+function ResizeBodyHeight()
+{
+    document.body.style.height = (MainDiv.height + 500).toString() + 'px';
+}*/
+
+$(document).ready(function () {
+    MainDiv = $('MainDiv');
+    var WindowOffset = $(window).scrollTop();
+    AllImages = $("img");
+    jQuery.each(AllImages, function (Index, Element) {
+        if (WindowOffset >= (Element.offsetTop - ($(window).height() / 2))) {
+            $(Element).animate({ opacity: '1' }, 1000);
+        }
+    });
+    $(window).scroll(function () {
+        var currentoffset = $(this).scrollTop();
+        console.log(currentoffset);
+        jQuery.each(AllImages, function (Index, Element) {
+            if (currentoffset >= (Element.offsetTop - ($(window).height()/2)))
+            {
+                $(Element).animate({ opacity: '1' }, 1000);
+            }
+        });
+    })
+});
