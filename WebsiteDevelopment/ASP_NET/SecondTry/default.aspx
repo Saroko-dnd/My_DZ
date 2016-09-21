1f61579cb4259952ea8632cc02569be92e3d1309
saroko-dnd <isaroko@mail.ru>
2016-09-21 17:28:42 +0300

    added another asp_net test project

---
+++ WebsiteDevelopment/ASP_NET/SecondTry/default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="default.aspx.cs" Inherits="MainWebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link href="CSSfolder/MainStyleSheet.css" rel="stylesheet"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="DivForTestOfTextInputAndOutput">
+        <p>Выберите цвет текста:</p>
+        <asp:DropDownList runat="server" AutoPostBack="true" ID="DropDownListOfColors" OnSelectedIndexChanged="DropDownListOfColors_SelectedIndexChanged">
+        </asp:DropDownList>
+        <asp:Label runat="server" id="MainTestLabel"></asp:Label>
+        <asp:TextBox runat="server" ID="MainInputTextBox"></asp:TextBox>
+        <asp:Button runat="server" Text="Add text to label" id="AddTextButton" OnClick="AddTextButton_Click"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebsiteDevelopment/ASP_NET/SecondTry/default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MainWebForm : System.Web.UI.Page
+{
+    static StringBuilder StringBuilderForTestLabel = new StringBuilder("");
+    static object GatesToStringBuilder = new object();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!(sender as Page).IsPostBack)
+        {
+            string[] allColors = Enum.GetNames(typeof(System.Drawing.KnownColor));
+            string[] systemEnvironmentColors =new string[(typeof(System.Drawing.SystemColors)).GetProperties().Length];
+
+            int index = 0;
+
+            foreach (MemberInfo member in (
+                typeof(System.Drawing.SystemColors)).GetProperties())
+            {
+                systemEnvironmentColors[index++] = member.Name;
+            }
+
+            List<string> finalColorList = new List<string>();
+
+            foreach (string color in allColors)
+            {
+                if (Array.IndexOf(systemEnvironmentColors, color) < 0)
+                {
+                    finalColorList.Add(color);
+                }
+            }
+            DropDownListOfColors.DataSource = finalColorList;
+            DropDownListOfColors.DataBind();
+            MainTestLabel.Text = "Test of ASP Label control.";
+        }
+    }
+
+    protected void AddTextButton_Click(object sender, EventArgs e)
+    {
+        lock(GatesToStringBuilder)
+        {
+            StringBuilderForTestLabel.Append("<br/>");
+            StringBuilderForTestLabel.Append(MainInputTextBox.Text);
+        }
+        MainTestLabel.Text = StringBuilderForTestLabel.ToString();
+    }
+
+    protected void DropDownListOfColors_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        MainTestLabel.ForeColor = Color.FromName((sender as DropDownList).SelectedItem.Text);
+    }
+}
