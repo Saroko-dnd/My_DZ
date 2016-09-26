205f498e3c87ae4a8f63eda832142caa3f03e4a1
unknown <admin@A2-6.itstep.lan>
2016-09-26 21:28:41 +0300

    minor changes in C# folder

---
+++ C#/WCF/BrakeSSHProject/WCFserverForBrakingSSH/WCFserverForBrakingSSH/ServerForBreakingSSh.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFserverForBrakingSSH
+{
+    
+    [ServiceContract]
+    public interface IServerForBreakingSSh
+    {
+        [OperationContract]
+        public MyTask GiveTask();
+
+        [OperationContract]
+        public void TakeAnswer(MyTask My7Tas_);
+
+    }
+
+    public class OurServer : IServerForBreakingSSh
+    {
+
+        public void TakeAnswer(MyTask My7Tas_)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyTask GiveTask()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ C#/WCF/BrakeSSHProject/WCFserverForBrakingSSH/WCFserverForBrakingSSH/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+
+namespace WCFserverForBrakingSSH
+{
+    [DataContract]
+    public class MyTask
+    {
+        [DataMember]
+        long Ot;
+        [DataMember]
+        long Do;
+        [DataMember]
+        string Hash;
+    }
+}
