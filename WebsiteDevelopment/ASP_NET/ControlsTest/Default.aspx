961ffdedc39b464d2241d763e1045b7c7a5e2306
unknown <admin@A2-6.itstep.lan>
2016-09-28 21:41:52 +0300

    first task asp net

---
+++ WebsiteDevelopment/ASP_NET/ControlsTest/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="CSSfolder/MainStyleSheet.css" rel="stylesheet"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="FlexColumn">
+            <div class="FlexRow">
+                <div class="FlexColumn">
+                    <asp:Label runat="server">Products</asp:Label>
+                    <asp:ListBox Width="300px" runat="server" Id="ListBoxProducts" SelectionMode="Multiple"></asp:ListBox>
+                </div>
+                <div class="FlexColumn">
+                    <asp:Label runat="server">Basket</asp:Label>
+                    <asp:ListBox Width="300px" runat="server" Id="ListBoxSelectedProducts" SelectionMode="Multiple"></asp:ListBox>
+                </div>
+            </div>
+            <div class="FlexColumn">
+                <asp:Button runat="server" Text="Add selected to basket" OnClick="AddSelectedToBasketButtonClick"/>
+                <asp:Button runat="server" Text="Remove selected from basket"/>
+                <asp:Button runat="server" Text="Add all to basket"/>
+                <asp:Button runat="server" Text="Remove all from basket"/>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebsiteDevelopment/ASP_NET/ControlsTest/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!(sender as Page).IsPostBack)
+        {
+            ListBoxProducts.DataSource = new List<string>() { "Product_1", "Product_2", "Product_3", "Product_4", "Product_5" };
+            ListBoxProducts.DataBind();
+        }
+    }
+
+    protected void AddSelectedToBasketButtonClick(object sender, EventArgs e)
+    {
+        ListItemCollection ItemsForBasketInProductsListBox = new ListItemCollection();
+        foreach (ListItem CurrentItem in this.ListBoxProducts.Items)
+        {
+            if (CurrentItem.Selected)
+            {
+                this.ListBoxSelectedProducts.Items.Add(CurrentItem);
+                ItemsForBasketInProductsListBox.Add(CurrentItem);
+            }
+        }
+        foreach (ListItem CurrentItem in ItemsForBasketInProductsListBox)
+        {
+            this.ListBoxProducts.Items.Remove(CurrentItem);
+        }  
+    }
+    protected void RemoveSelectedFromBasketButtonClick(object sender, EventArgs e)
+    {
+        ListItemCollection ItemsForRemoveActionInBasketListBox = new ListItemCollection();
+        foreach (ListItem CurrentItem in this.ListBoxSelectedProducts.Items)
+        {
+
+        }
+        foreach (ListItem CurrentItem in ItemsForRemoveActionInBasketListBox)
+        {
+
+        }  
+    }
+}
