0ee989cc35fa0b2b2879a95cfa37559c876b400c
saroko-dnd <isaroko@mail.ru>
2016-10-28 04:22:40 +0300

    update for MVCChat

--- WebsiteDevelopment/ASP_NET/MVC_PROJECTS/SimpleChat/SimpleChat/Models/UserChatPage.cs
+++ WebsiteDevelopment/ASP_NET/MVC_PROJECTS/SimpleChat/SimpleChat/Models/UserChatPage.cs
@@ -7,6 +7,10 @@ namespace SimpleChat.Models
 {
     public class UserChatPage
     {
+        private static readonly string sortMessagesByUserName = "Sort messages by user name";
+        private static readonly string sortMessagesByDate = "Sort messages by date";
+        private static readonly string doNotSortMessages = "No sorting";
+
         private string userName = "User";
 
         public string UserName
@@ -22,12 +26,51 @@ namespace SimpleChat.Models
             set { userMessage = value; }
         }
 
+        private string sortingForMessages = DoNotSortMessages;
+
+        public string SortingForMessages
+        {
+            get
+            {
+                return sortingForMessages;
+            }
+
+            set
+            {
+                sortingForMessages = value;
+            }
+        }
+
         private List<UserMessage> lastMessages;
 
         public List<UserMessage> LastMessages
         {
             get { return lastMessages; }
             set { lastMessages = value; }
+        }
+
+        public static string SortMessagesByUserName
+        {
+            get
+            {
+                return sortMessagesByUserName;
+            }
+        }
+
+        public static string SortMessagesByDate
+        {
+            get
+            {
+                return sortMessagesByDate;
+            }
+        }
+
+        public static string DoNotSortMessages
+        {
+            get
+            {
+                return doNotSortMessages;
+            }
         }
     }
 }
