e13622821167f81faa3e1940f2f340e17dbdf1e8
saroko-dnd <isaroko@mail.ru>
2016-11-20 04:08:27 +0200

    update for asp exam project(added new controller and page for login and registration actions)

---
+++ WebsiteDevelopment/ASP_NET/EXAM/Race/RaceWebsite/Controllers/LoginAndRegistrationForAdminsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RaceWebsite.Controllers
+{
+    public class LoginAndRegistrationForAdminsController : Controller
+    {
+        // GET: LoginAndRegistration
+        public ActionResult PageForLoginAndRegistration()
+        {
+            return View();
+        }
+
+        public JsonResult AttemptToRegisterNewAdmin(string NewAdminName, string NewAdminPassword)
+        {
+            return null;
+        }
+    }
+}
