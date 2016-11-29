3efb4e668ca859e0cc432431511c8207439bac82
saroko-dnd <isaroko@mail.ru>
2016-11-29 04:18:33 +0200

    small update for php FirstTry project

--- WebsiteDevelopment/PHP/FirstTry/TestProject.php
+++ WebsiteDevelopment/PHP/FirstTry/TestProject.php
@@ -3,7 +3,11 @@
 echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+$TestHtmlReference = htmlspecialchars("This is reference after htmlspecialchars() <a href='test'>Test reference</a>", ENT_QUOTES);
+$PureHtmlReference = "This is usual reference <a href='test'>Test reference</a>";
+echo $PureHtmlReference, "<br>";
+echo $TestHtmlReference, "<br>";
 ?> 
 
  <form action="">
