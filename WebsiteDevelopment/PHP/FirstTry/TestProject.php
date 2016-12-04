2d2094632575d2a2d314df8b737e243e66bfcbd4
saroko-dnd <isaroko@mail.ru>
2016-12-04 03:56:27 +0200

    update for php FirstTry project

--- WebsiteDevelopment/PHP/FirstTry/TestProject.php
+++ WebsiteDevelopment/PHP/FirstTry/TestProject.php
@@ -20,11 +20,69 @@ echo $TestHtmlReference, "<br>";
   <input type="checkbox" name="TestCheckBox" value="Checkbox value">
  </form>
 
+ <form action="">
+     <p>Enter manufacturer name to get info about all his products:</p>
+     <input type="text" name="SelectedProductManufacturer">      
+     <input type="submit" value="Найти">
+ </form>
+
+ <?php
+ //SELECT EXAMPLE with binding
+  if(isset($_GET['SelectedProductManufacturer'])) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "firsttrydb";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);  
+
+        $stmt = $conn->prepare("SELECT * FROM products WHERE Manufacturer=?");
+        $stmt->bind_param('s', $SelectedProductManufacturer);
+        $SelectedProductManufacturer = $_GET['SelectedProductManufacturer'];
+
+        $stmt->execute();
+        $stmt->bind_result($ProductID, $ProductName, $ManufacturerOfProduct, $ProductCost);
+        while ($stmt->fetch()) {
+            printf("ID: %d Name: %s Manufacturer: %s Cost: %d<br>", $ProductID, $ProductName, $ManufacturerOfProduct, $ProductCost);
+        }
+        $stmt->close();
+
+        $conn->close();
+    }
+ ?>
+
+<h1>Addition of new products to database</h1>
+<form action="">
+    <p>Name of new product</p>
+    <input type="text" name="NewProductName"> 
+    <p>Manufacturer of new product</p>
+    <input type="text" name="NewProductManufacturer"> 
+    <p>Cost of new product</p>
+    <input type="number" name="NewProductCost"> 
+    <input type="submit" value="Add new product to database">
+</form>
+
 <?php
- print_r($_GET);
- //If value with name 'TestCheckBox' exists then echo
- if(isset($_GET['TestCheckBox'])) {
-        echo "Check box was checked";
+ //print_r($_GET);
+//INSERT EXAMPLE with binding
+ if(isset($_GET['NewProductName']) && isset($_GET['NewProductManufacturer']) && isset($_GET['NewProductCost'])) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "firsttrydb";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);  
+
+        $stmt = $conn->prepare("INSERT INTO products (Name, Manufacturer, Cost) VALUES (?, ?, ?)");
+        $stmt->bind_param('ssd', $NewProductName, $NewProductManufacturer, $NewProductCost);
+        $NewProductName = $_GET['NewProductName'];
+        $NewProductManufacturer = $_GET['NewProductManufacturer'];        
+        $NewProductCost = $_GET['NewProductCost'];
+
+        $stmt->execute();
+        $stmt->close();
+
+        $conn->close();
     }
 ?>
 
