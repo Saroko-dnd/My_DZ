4524c34362e5e829664ff754b987cde193811e71
unknown <admin@A3-12.itstep.lan>
2016-12-10 13:24:01 +0300

    update for php project

--- WebsiteDevelopment/PHP/FirstTry/TestProject.php
+++ WebsiteDevelopment/PHP/FirstTry/TestProject.php
@@ -28,6 +28,7 @@ echo $TestHtmlReference, "<br>";
  <?php
  //SELECT EXAMPLE with binding
   if(isset($_GET['SelectedProductManufacturer'])) {
+        CreateDbIfNotExist();
         $servername = "localhost";
         $username = "root";
         $password = "";
@@ -64,11 +65,12 @@ echo $TestHtmlReference, "<br>";
  //print_r($_GET);
 //INSERT EXAMPLE with binding
  if(isset($_GET['NewProductName']) && isset($_GET['NewProductManufacturer']) && isset($_GET['NewProductCost'])) {
+        CreateDbIfNotExist();
         $servername = "localhost";
         $username = "root";
         $password = "";
         $dbname = "firsttrydb";
-
+        
         $conn = new mysqli($servername, $username, $password, $dbname);  
 
         $stmt = $conn->prepare("INSERT INTO products (Name, Manufacturer, Cost) VALUES (?, ?, ?)");
@@ -82,6 +84,42 @@ echo $TestHtmlReference, "<br>";
 
         $conn->close();
     }
+
+    function CreateDbIfNotExist()
+    {
+        //phpinfo();
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "";
+        //$dbname = "firsttrydb";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if(!$conn->select_db("firsttrydb"))
+        {
+            $SqlQuery = 'CREATE DATABASE firsttrydb';
+            $conn->query($SqlQuery);
+            $conn->close();  
+            $conn = new mysqli($servername, $username, $password, "firsttrydb");
+            $SqlQuery = "CREATE TABLE products (
+                ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+                Name VARCHAR(50) NOT NULL,
+                Manufacturer VARCHAR(50) NOT NULL,
+                Cost INT UNSIGNED
+                )";
+            $conn->query($SqlQuery);       
+        }
+        $conn->close();  
+    }
+
+        //Test of classes in php
+    class TestClass
+    {
+        public $TestVar = "Blablabla";
+    }
+
+    $TestObject = new TestClass();
+    echo $TestObject->TestVar;
 ?>
 
 
